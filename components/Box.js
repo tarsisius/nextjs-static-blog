93d91549b3484845bd00ef99c62b5d4126a67462
Tarsisius Haryo <trsharyo@gmail.com>
2021-10-26 19:57:36 +0700

    Minor Update

--- components/Box.js
+++ components/Box.js
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import Link from "next/link";
 import Category from "@/components/Category";
 
 export default function Box({ slug, title, categories, excerpt }) {
+  const [cat, showCat] = useState(false);
+
   return (
     <div className="box">
-        <Category categories={categories} />
-        <Link href={"/" + slug} passHref>
-          <a>
-            <h3 className="heading">{title}</h3>
-          </a>
-        </Link>
-        <span className="excerpt">{excerpt}</span>
+      <Link href={"/" + slug} passHref>
+        <a>
+          <h3 className="heading">{title}</h3>
+        </a>
+      </Link>
+      <div className="content">
+        <div className="changable">
+          {cat && <Category categories={categories} />}
+          <p className="excerpt">{excerpt}</p>
+        </div>
+        <div className="icon">
+          <span className="icon__child" onClick={() => showCat((e) => !e)}>
+            <img className="inner" src="/assets/svg/hash.svg" alt="Kategori" />
+          </span>
+        </div>
       </div>
+    </div>
   );
 }
